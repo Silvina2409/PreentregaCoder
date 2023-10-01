e348ee584bd9fa7cbe7bfe6e5a242ac700c5bdca
Silvina2409 <practicasilvi@gmail.com>
2023-10-01 18:15:49 -0300

    primera entrega

---
+++ script.js
@@ -0,0 +1,49 @@
+const usuario = prompt ("Ingrese su usuario") // El usuario es juan
+const contrasena = prompt ("Ingrese su contraseña") // La contraseña es 1234
+
+const Registrar = () =>{
+    let lista = []
+    let aviso = prompt ("¿Desea realizar una consulta médica? Escriba SI o NO").toUpperCase()
+    while (aviso == "SI"){
+        let consulta = prompt ("Ingrese el tipo de consulta  que desea realizar: INFECTOLOGIA, NUTRICION  u OFTALMOLOGIA").toUpperCase()
+        lista.push(consulta)
+        console.log (lista)
+        aviso = prompt ("Desea agregar otra consulta. Indique SI o NO").toUpperCase()
+    }
+    let total = 0
+    
+    for (consulta of lista){
+        switch (consulta) {
+            case "INFECTOLOGIA":
+                alert ("El costo de la consulta de infectologia es de $5000")
+                total = total + 5000
+                break;
+            case "NUTRICION":
+                alert ("El costo de la consulta de nutrición es de $8000")
+                total = total + 8000
+                break;
+            case "OFTALMOLOGIA":
+                alert ("El costo de la consulta de oftalmología es de $9000")
+                total = total + 9000
+                break;    
+            default:
+                let anuncio = alert("No contamos con esa especialidad o está mal escrita")
+                break;
+        }
+    
+    }
+alert (`El total a abonar es ${total} pesos`) 
+    
+}
+
+
+
+
+function Confirmar (){
+    if (usuario == "juan" && contrasena == 1234){
+        alert ("Usuario y contraseña correctos, puede ingresar")
+        Registrar()
+    }else {
+        alert ("Usuario y/o contraseña incorrectos, no puede ingresar")
+}}
+Confirmar()      
